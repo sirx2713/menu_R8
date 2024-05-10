fe8620263b7dea8f5f9ef03916b5a1113eb8f719
Mabende <belindamabende@gmail.com>
2024-05-10 18:06:56 +0200

    Initial Commit

---
+++ menu_R8/menu_R8App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_R8App.swift
+//  menu_R8
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/10.
+//
+
+import SwiftUI
+
+@main
+struct menu_R8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
